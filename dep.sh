83303e57eb82b9571977f4a12ca261f9532d3a60
phucnh7 <phucnh7@vng.com.vn>
2024-08-11 12:10:15 +0700

    add git pull cmd

--- dep.sh
+++ dep.sh
@@ -1,2 +1,3 @@
+git pull https://nguyenhongphuc98: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@github.com/Nguyenhongphuc98/Export-manager-Web.git
 docker build -t em-web .
 docker run -p 8081:3000 em-web
