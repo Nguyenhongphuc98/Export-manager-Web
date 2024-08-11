91b2394923f4e4f4fbadd52c7149c01586dfc41c
phucnh7 <phucnh7@vng.com.vn>
2024-08-11 11:45:22 +0700

    chore: add deploy cmd

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,8 +34,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-/**
- * docker build -t my-nextjs-app .
- * docker run -p 3000:3000 my-nextjs-app
- */
---
+++ dep.sh
@@ -0,0 +1,2 @@
+docker build -t em-web .
+docker run -p 8081:3000 em-web
